3c70016cfe78c8a7478a5d125c0d4bc011e5b114
kayigmb <146448328+kayigmb@users.noreply.github.com>
2024-01-31 16:47:14 +0200

    fix-mnav-bar

--- Admin/addpost.js
+++ Admin/addpost.js
@@ -3,8 +3,42 @@ const title = document.getElementById('title');
 const image = document.getElementById('image');
 const error = document.getElementById('error');
 const content = document.getElementById('content');
+const btn = document.getElementById('btn');
 
 
-contentEnter = content.textContent();
 
-console.log(contentEnter);
+
+btn.onclick = (e) =>{
+    e.preventDefault();
+    validateForm();
+}
+
+function validateForm() {
+    clearErrorMessage();
+    
+    const titleEnter = title.value;
+    const imageEnter = image.value.trim();
+    // const contentEnter = content.value.trim();
+
+    if(titleEnter === ''){
+        errorMessage("Please enter a title");
+        console.log('vali')     
+    }
+    if(imageEnter ===''){
+        errorMessage("Please enter an image");
+    }
+    // if (contentEnter == ''){
+    //     errorMessage("Please enter a content");
+    // }
+
+}
+
+function clearErrorMessage() {
+    error.innerHTML = ''
+}
+
+function errorMessage(message){
+    const errorWord = document.createElement("p");
+    errorWord.innerHTML = message;
+    error.appendChild(errorWord);
+}
