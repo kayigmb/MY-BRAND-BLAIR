73ab83184e81a66ec345a605961235a6d8ef5f70
kayigmb <146448328+kayigmb@users.noreply.github.com>
2024-03-13 13:39:45 +0200

    ft-react

--- Pages/blog_pages.js
+++ Pages/blog_pages.js
@@ -1,115 +1,81 @@
-const hmenu = document.querySelector('.hmenu');
-        const mnav = document.querySelector('.mnav');
-        const menuItems = document.querySelectorAll('.mnav a');
-        
-        hmenu.addEventListener('click',function() {
-            hmenu.classList.toggle('is-active');
-            mnav.classList.toggle('is-active');
-        });
-        menuItems.forEach(function(menuItem) {
-                  menuItem.addEventListener('click', function() {
-                 hmenu.classList.remove('is-active');
-                 mnav.classList.remove('is-active');
-});
-});
+const {useState,useEffect} = React
 
-// view Blogs
+function App(){
 
-const viewBlogs = () =>{
+    const [posts,setPosts] = useState([])
 
-    axios({
-        
-        url:"https://mybrand-be-4hmq.onrender.com/api/blogs"
+    useEffect(()=>{
+        axios({
+            url:"https://mybrand-be-4hmq.onrender.com/api/blogs"
+        }).then((res)=>{
+                setPosts(res.data)
+        }).catch((err)=>{
+            console.error(err);
+        })
+    },[])
 
-    }).then((res)=>{
-        const container = document.querySelector('.blog_cards_container');
 
-        const posts = res.data || []
-        
-                posts.forEach((element,index) =>{
-                      
-                    const blogCard = document.createElement('div');
-                    const img = document.createElement('img');
-                    const blogWord = document.createElement('div');
-                    const h2 = document.createElement('h2');
-                    const p1 = document.createElement('p');
-                    const p2 = document.createElement('p');
-                    const a = document.createElement('a')
+    function OpenLink(id){
+        localStorage.setItem('blogCurrent', id)
+        window.location.href = 'blog_one.html'
+        // console.log(id)
+    }
 
-                    const iconReact = document.createElement('div')
-                    const icons = document.createElement('div')
-                    const react1 = document.createElement('div')
-                    const react2 = document.createElement('div')
-                        // class names
 
-                        blogCard.className = 'blog_card'
-                        blogWord.className = 'blog_word'
-                        a.className = 'learn_more'
+    const Card=({single})=>{
 
-                        iconReact.className = 'icons_reacticon'
-                        icons.className = 'iconsreact'
-                        react1.className = 'reaction1'
-                        react2.className = 'reaction2'
-                    //assign
-                    h2.innerText = element.title;
-                    img.src = element.image;
-                    
-                    a.innerHTML = `<a onclick='viewBlog("${element._id}")' class="learn_more">Learn More 
-                    <span style="font-weight: bold;">></span>
-                        </a>`
+        const [count, setCount] = useState(0); 
 
+        useEffect(async () => {
+                try{
                     axios({
-                            url: `https://mybrand-be-4hmq.onrender.com/api/blogs/${element._id}/likes`
-                        }).then((res)=>{
-
-                            react1.innerHTML = `<i class="far fa-thumbs-up"></i><p>${res.data.likes}</p>`
-                            
-                    })
-            
-                        
-            
-                    axios({
-                        url: `https://mybrand-be-4hmq.onrender.com/api/blogs/${element._id}/comments`
+                        url: `https://mybrand-be-4hmq.onrender.com/api/blogs/${single._id}/comments`
                     }).then((res)=>{
-            
-
-                        react2.innerHTML = `<i class="fa-solid fa-comment"></i><p>${res.data.length}</p>`
-
+                        setCount(res.data.length)
+                    }).catch((err)=>{
+                        console.error(err)
                     })
+                }catch(err){
+                    console.log(err)
+                }
+        }, [single._id]);
 
-                    container.appendChild(blogCard);
+            return ( 
 
-                    blogCard.appendChild(img)   
-                    blogCard.appendChild(blogWord);
-                    blogCard.appendChild(iconReact)
+                <div className="blog_card">
+                    <img src={single.image} alt="" />
+                    <div className="blog_word">
+                        <h2 id="title">{single.title}</h2>
+                        <a href="blog_one.html" class="learn_more" onClick={()=>OpenLink(single._id)}>Learn More  
+                                <span>{" ->"}</span>
+                        </a>
+                    </div>
+                    <div className="icons_reactions">
+                        <div className="iconsreact">
+                            <div className="reaction1"><i class="far fa-thumbs-up"></i><p>{single.likes.length}</p></div>
+                        <div className="reaction2"> <i class="fa-solid fa-comment"></i><p>{count}</p></div>
+                        </div>
+                    </div>
+                </div>
+            )         
+        }
 
-                    blogWord.appendChild(h2);
-                    // blogWord.appendChild(p1);
-                    blogWord.appendChild(a);
+ 
 
-                    iconReact.appendChild(icons)
-                    icons.appendChild(react1);
-                    icons.appendChild(react2);
+    return (
+        (posts.length >0 ? (posts.map((e) => (
+            <div key={e._id}>
+              <Card key={e._id} single={e} />
+            </div>
+        ))):(
+            <div>
+                <h3>Blogs loading ...</h3>
+            </div>
+        ))
+    )
 
-                
-            })
-
-    })
-
-
-}
-
-viewBlogs()
-
-
-
-// single blog index
-
-function viewBlog(index){
     
-    const currentBlog = sessionStorage.setItem('blogCurrent', index)
-    window.location.href = 'blog_one.html'
-    // console.log(index)
-
 }
+
+ReactDOM.render(<App />, document.querySelector('.blog_cards_container'))
 
